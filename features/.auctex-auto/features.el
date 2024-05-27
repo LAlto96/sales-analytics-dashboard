c8e1679066ef047a1e661657df3fdacb66de3401
Alto <lafon.ludovic.ll@proton.me>
2024-05-27 12:13:35 +0200

    Add features explantion file

---
+++ features/.auctex-auto/features.el
@@ -0,0 +1,15 @@
+(TeX-add-style-hook
+ "features"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("inputenc" "utf8")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "article"
+    "art10"
+    "inputenc"
+    "hyperref"
+    "geometry"
+    "lipsum"))
+ :latex)
+
